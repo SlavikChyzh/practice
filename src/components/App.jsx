0786e782b9763279d5fb373fc40eb28810d1327b
Andrii Popov <popov.andrey80@gmail.com>
2023-03-25 12:58:43 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,64 @@
+import { useState, useEffect } from 'react';
+import { Button } from './Button/Button';
+import { fetchMovies } from '../services/moviesApi';
+import { List } from './List/List';
+import { Loader } from './Loader/Loader';
+
+export const App = () => {
+  const [isListShow, setIsListShow] = useState(false);
+  const [page, setPage] = useState(1);
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const queryMovies = () => {
+      setIsLoading(true);
+      fetchMovies(page)
+        .then(response => {
+          setMovies(prevState => [...prevState, ...response.data.results]);
+        })
+        .catch(error => {
+          console.log('error :>> ', error);
+        })
+        .finally(() => {
+          setIsLoading(false);
+        });
+    };
+
+    if (isListShow) {
+      queryMovies();
+    }
+
+    if (!isListShow) {
+      setMovies([]);
+      setPage(1);
+    }
+  }, [isListShow, page]);
+
+  const showList = () => {
+    setIsListShow(prevState => !prevState);
+  };
+
+  const loadMore = () => {
+    setPage(prevState => prevState + 1);
+  };
+
+  const deleteItem = id => {
+    setMovies(prevState => [prevState.filter(item => item.id !== id)]);
+  };
+
+  return (
+    <div>
+      {isLoading && <Loader />}
+      <Button text={isListShow ? 'hide List' : 'show List'} clickHandler={showList} />
+      <List movies={movies} deleteMovie={deleteItem} />
+      {movies.length !== 0 && <Button text={'load more...'} clickHandler={loadMore} />}
+    </div>
+  );
+};
+
+// =============================================================
+
 // import { Component } from 'react';
 
 // import { Button } from './Button/Button';
@@ -69,65 +130,3 @@
 //     );
 //   }
 // }
-
-//============
-
-import { useState, useEffect } from 'react';
-
-import { Button } from './Button/Button';
-import { fetchMovies } from '../services/moviesApi';
-import { List } from './List/List';
-import { Loader } from './Loader/Loader';
-
-export const App = () => {
-  const [isListShow, setIsListShow] = useState(false);
-  const [page, setPage] = useState(1);
-  const [movies, setMovies] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    const queryMovies = () => {
-      setIsLoading(true);
-      fetchMovies(page)
-        .then(response => {
-          setMovies(prevState => [...prevState, ...response.data.results]);
-        })
-        .catch(error => {
-          console.log('error :>> ', error);
-        })
-        .finally(() => {
-          setIsLoading(false);
-        });
-    };
-
-    if (isListShow) {
-      queryMovies();
-    }
-
-    if (!isListShow) {
-      setPage(1);
-      setMovies([]);
-    }
-  }, [isListShow, page]);
-
-  const showList = () => {
-    setIsListShow(prevState => !prevState);
-  };
-
-  const loadMore = () => {
-    setPage(prevState => prevState + 1);
-  };
-
-  const deleteItem = id => {
-    setMovies(prevState => [prevState.filter(item => item.id !== id)]);
-  };
-
-  return (
-    <div>
-      {isLoading && <Loader />}
-      <Button text={!isListShow ? 'show list' : 'hide List'} clickHandler={showList} />
-      <List movies={movies} deleteMovie={deleteItem} />
-      {movies.length !== 0 && <Button text={'load more...'} clickHandler={loadMore} />}
-    </div>
-  );
-};
